c62a16d18a0ea2703a6951ad06f45cf60fc0e6e9
B Kleinen <kleinen@infrastructure.de>
2018-07-03 14:49:09 +0200

    catching the FileNotFoundException

--- v2/CountWords/CountWords.java
+++ v2/CountWords/CountWords.java
@@ -1,25 +1,36 @@
 import java.io.FileReader;
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 
 public class CountWords{
 
-    public static void main(String[] args) throws Exception{
+    public static void main(String[] args) throws Exception {
 
         if (args.length > 0){
             String fileName = args[0];
-            FileReader fr = new FileReader(fileName);
-            BufferedReader rd = new BufferedReader(fr);
-            String line = "";
-            int count = 0;
-            while (line != null){
-                line = rd.readLine();
-                count++;
-                System.out.println(line);
+            FileReader fr = null;
+            try {
+                fr = new FileReader(fileName);
+
+                BufferedReader rd = new BufferedReader(fr);
+                String line = "";
+                int count = 0;
+                while (line != null){
+                    line = rd.readLine();
+                    count++;
+                    System.out.println(line);
+                }
+                System.out.println("The file had "+count+" lines");
+
+                // close file
+                rd.close();
+                fr.close();
+            } 
+            catch(FileNotFoundException e){
+                System.out.println("File not found: "+fileName);
+                System.out.println(e.getMessage());
             }
-            System.out.println("The file had "+count+" lines");
-            // close file
-            rd.close();
-            fr.close();
+
         }
         else
             System.out.println("usage: java CountWords <filename>");
